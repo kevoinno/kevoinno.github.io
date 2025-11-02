e2966493df3212c62b082993a9ca5abd0da5f278
Kevin Ngo <kevoin.ngo@gmail.com>
2025-11-01 21:10:59 -0700

    astro revamp

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://kevoinno.github.io',
+    base: '/',
+    output: 'static'
+});
